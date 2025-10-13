c62d18e5223c4f6e8bcbf245a1cd5d50dcbb6160
Luana Calderon <130126957+LuanaCalderon@users.noreply.github.com>
2025-10-12 22:19:17 -0300

    Modificacion en rutas

--- src/routes/recipe.routes.js
+++ src/routes/recipe.routes.js
@@ -2,8 +2,9 @@ const { Router } = require("express");
 const router = Router();
 const RecipeControllers = require("../controllers/recipe.controller");
 
-router.get("/", RecipeControllers.getRecipes);
-router.get("/:id", RecipeControllers.getRecipesByName);
+router.get("/", RecipeControllers.getRecipes); 
+router.get("/buscar", RecipeControllers.getRecipesByName); 
+router.get("/:id", RecipeControllers.getRecipeById); 
 router.post("/", RecipeControllers.createRecipe);
 router.put("/:id", RecipeControllers.updateRecipe);
 router.delete("/:id", RecipeControllers.deleteRecipe);
