c12562111927cbbb38044ebd478edcdaf1266055
Luana Calderon <130126957+LuanaCalderon@users.noreply.github.com>
2025-10-13 14:01:47 -0300

    Cambios en el main

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+
 const db = require("./db/models");
 const rutaAfiliados = require("./routes/affiliate.routes");
 const rutaRecetas = require("./routes/recipe.routes");
 
 const affiliate = require("./db/models/affiliate");
-const recipe = require("./db/models/recipe")
+const recipe = require("./db/models/recipe");
 
 const app = express();
 const PORT = 3000;
-
 
 // Middlewares
 app.use(cors());
@@ -19,16 +19,13 @@ app.use(express.json());
 app.use("/affiliate", rutaAfiliados);
 app.use("/recipes", rutaRecetas);
 
-
-
 const start = async () => {
   try {
     await db.sequelize.authenticate();
     console.log("✔️ Conexión a la base de datos OK");
-    
+
     await db.sequelize.sync({ alter: true });
     console.log("✔️ Generación de tablas OK");
-
 
     app.listen(PORT, () => {
       console.log(`🚀 La app arrancó en el puerto ${PORT}`);
@@ -39,5 +36,4 @@ const start = async () => {
 };
 
 start();
-
 
