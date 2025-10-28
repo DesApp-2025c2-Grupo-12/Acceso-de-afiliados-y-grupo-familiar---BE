9a187b2c50f6ae5f6c9be754084ff59071d747b5
Luana Calderon <130126957+LuanaCalderon@users.noreply.github.com>
2025-10-27 21:23:26 -0300

    Se modificaron los models de recetas (recipes)

--- src/db/models/recipe.js
+++ src/db/models/recipe.js
@@ -1,31 +1,27 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Recipe extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
-Recipe.init({
+
+  Recipe.init({
     nombreDelMedicamento: { type: DataTypes.STRING, allowNull: false },
     presentacion: { type: DataTypes.STRING, allowNull: false },
     paciente: { type: DataTypes.STRING, allowNull: false },
     numeroDeDocumento: { type: DataTypes.STRING, allowNull: false },
-    fechaDeEmision: { type: DataTypes.DATEONLY, allowNull: false },
+    fechaDeEmision: { type: DataTypes.DATEONLY, allowNull: true }, // allowNull ahora es true
     cantidad: { type: DataTypes.INTEGER, allowNull: false },
     estado: { type: DataTypes.STRING, allowNull: false },
     observaciones: { type: DataTypes.STRING, allowNull: false }
-}, {
+  }, {
     sequelize,
     modelName: 'Recipe',
     timestamps: true
-});
+  });
+
   return Recipe;
 };
