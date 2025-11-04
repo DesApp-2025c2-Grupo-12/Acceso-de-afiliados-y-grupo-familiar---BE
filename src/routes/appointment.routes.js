a9b67f6fc75d1370c99d8c4f9575e375a4d280ba
GabrielFacundoGutierrez <gabrielfacundo.gutierrez@estudiantes.unahur.edu.ar>
2025-11-03 22:35:21 -0300

    se agrego la funcion turnosFuturos de un afiliado en appointments controllers y rutas, en el package.json se agregaron comando en scripts para que se puedan quitar todos los datos que agregaban los seeders.

--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -6,6 +6,7 @@ router.get("/", AppointmentControllers.getAppointments);
 router.get("/unreserved-appointments/",AppointmentControllers.unreservedAppointments)
 router.get("/especialidades/",AppointmentControllers.especialidadesDisponibles)
 router.get("/:id", AppointmentControllers.getAppointmentById);
+router.get("/turnosFuturos/:id",AppointmentControllers.turnosFuturosDeAfiliado)
 router.get("/affiliated-appointments/:id",AppointmentControllers.apointmentsFromAffiliate)
 router.post("/", AppointmentControllers.createAppointment);
 router.put("/:id", AppointmentControllers.updateAppointment);
